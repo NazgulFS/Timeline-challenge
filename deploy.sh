2e5cea7fbed501734472d28f0dfab951ee8d8e74
Nazgûl <59717278+NazgulFS@users.noreply.github.com>
2020-10-08 02:16:15 -0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:NazgulFS/timeline.git master:gh-pages
+git push -f git@github.com:NazgulFS/timeline-challenge master:gh-pages
 
 cd -
